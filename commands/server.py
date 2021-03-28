9ee45c6b223a33a8d9d2825205b5015d51293d50
Unic X <73340774+Unic-X@users.noreply.github.com>
2021-03-28 15:16:27 +0530

    IDK

--- commands/server.py
+++ commands/server.py
@@ -6,7 +6,11 @@ from io import BytesIO
 def timetext(name):
     """ Timestamp, but in text form """
     return f"{name}_{int(time.time())}.txt"
-
+def date(target, clock=True):
+    """ Clock format using datetime.strftime() """
+    if not clock:
+        return target.strftime("%d %B %Y")
+    return target.strftime("%d %B %Y, %H:%M")
 
 class Server(commands.Cog):
     @commands.command()
@@ -51,6 +55,29 @@ class Server(commands.Cog):
                 message += f"{all_status[g]['emoji']} {', '.join(all_status[g]['users'])}\n"
 
         await ctx.send(f"Mods in **{ctx.guild.name}**\n{message}")
+    
+    @commands.group()
+    @commands.guild_only()
+    async def server(self, ctx):
+        """ Check info about current server """
+        if ctx.invoked_subcommand is None:
+            find_bots = sum(1 for member in ctx.guild.members if member.bot)
+
+            embed = dc.Embed()
+
+            if ctx.guild.icon:
+                embed.set_thumbnail(url=ctx.guild.icon_url)
+            if ctx.guild.banner:
+                embed.set_image(url=ctx.guild.banner_url_as(format="png"))
+
+            embed.add_field(name="Server Name", value=ctx.guild.name, inline=True)
+            embed.add_field(name="Server ID", value=ctx.guild.id, inline=True)
+            embed.add_field(name="Members", value=ctx.guild.member_count, inline=True)
+            embed.add_field(name="Bots", value=find_bots, inline=True)
+            embed.add_field(name="Owner", value=ctx.guild.owner, inline=True)
+            embed.add_field(name="Region", value=ctx.guild.region, inline=True)
+            embed.add_field(name="Created", value=date(ctx.guild.created_at), inline=True)
+            await ctx.send(content=f"ℹ information about **{ctx.guild.name}**", embed=embed)    
 
     @server.command(name="avatar", aliases=["sicon","serveric","guildicon","servericon"])
     async def server_avatar(self, ctx):
@@ -58,6 +85,7 @@ class Server(commands.Cog):
         if not ctx.guild.icon:
             return await ctx.send("This server does not have a avatar...")
         await ctx.send(f"Avatar of **{ctx.guild.name}**\n{ctx.guild.icon_url_as(size=1024)}")
+
 
 def setup(bot):
     bot.add_cog(Server())

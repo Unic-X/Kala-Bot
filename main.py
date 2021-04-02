a5607f74753e954b4bd4f2c8343191e35f61d7fb
Unic X <73340774+Unic-X@users.noreply.github.com>
2021-04-02 22:35:31 +0530

    Minor Fixes and im ded

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord as dc
 from discord.ext import commands
 from commands import utils
-#from keep_alive import keep_alive
-#keep_alive()
+from keep_alive import keep_alive
+keep_alive()
 
 intents=dc.Intents.default()
 intents.members=True

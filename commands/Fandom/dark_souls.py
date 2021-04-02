33276f2c9fe5632e61801628c1fd035b44b76788
Unic X <73340774+Unic-X@users.noreply.github.com>
2021-04-03 02:11:36 +0530

    Added Parsing

--- commands/Fandom/dark_souls.py
+++ commands/Fandom/dark_souls.py
@@ -65,13 +65,42 @@ class Page:
             "pageid": self.pageid,
             "contentformat": 'application/json'
         }
-        self.PARSED_DATA = _fandom_request(SEARCH_PARAMS)["parse"]
-        return self.PARSED_DATA
+        self.PARSED_DATA = _fandom_request(SEARCH_PARAMS)["parse"]["text"]["*"]
+        content = BeautifulSoup(self.PARSED_DATA, "lxml")
+        paragraphs = content.find_all(
+            "p", limit=10
+        )  # returns list of all <p> inside <div> class_=mw-parser-output we only need 1st(and 2nd if the content is less and even 3rd can be added if needed)
 
-    @property
-    def images(self):
-        self.PARSED_DATA
+        for par in paragraphs:
+            for tags in par.find_all(
+                    "sup"
+            ):  # deletes all the <sup> and returns a clean code without [] but has a time complexity of O(n^2)
+                tags.decompose()
 
+        counter = 0
+        while len(
+                paragraphs[counter].text
+        ) == 1:  # the loop will check until the paragraph is not empty
+            counter += 1
+
+        text_content = paragraphs[counter].text
+
+        if len(text_content) > 1024:
+            text_content = text_content[:1021] + '..'
+
+        for paragraph in paragraphs[
+                counter +
+                1:]:  # O(1) is more better than searching the whole list
+            if len(text_content) > 1024 or len(text_content +
+                                               paragraph.text) > 1024:
+                break  # breaks the loop if more than 1024 char for Embed
+            else:
+                text_content += "\n" + paragraph.text
+        return text_content
+
+    #@property
+    #def images(self):
+    #    self.PARSED_DATA
 
 class Search:
     def __init__(self):
@@ -104,6 +133,7 @@ class Search:
         fandom_content = _fandom_request(SEARCH_PARAMS)
         result = dict(zip(fandom_content[1], fandom_content[3]))
         return result
+        
 
 # page=Page(pageid=150086)
 # print(page.all_content)
@@ -136,3 +166,7 @@ class Fandom(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Fandom())
+
+update_fandom("darksouls")
+page = Page(pageid=52657)
+print(page.all_content)
